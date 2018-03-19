bbb73ea3665c03bae2d5037dfcbf932c670b1c14
Nandhini1826 <sparknandy@gmail.com>
2018-03-19 23:37:21 +0530

    itemsList page
    itemsList page has been created

--- src/main/webapp/app/entities/ext-bill/ext-bill.service.ts
+++ src/main/webapp/app/entities/ext-bill/ext-bill.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { isUndefined } from 'util';
+import { Item } from '../item/item.model';
 
 @Injectable()
 export class ExtBillService {
+constructor() { }
+  /* To fetch the items from local storage, 
+  if the items are not available in local storage then 
+  it calls a function to fill into local storage */
+  fetchItems() {
+    console.log('in fetch');
+    if(isUndefined(localStorage.getItem("items")) ||
+      localStorage.getItem("items") === null
+    ) {
+      this.fillItems();
+    } 
+  const itemsStr = localStorage.getItem("items");
+    // console.log('in fetch str = ' + itemsStr);
+    return JSON.parse(itemsStr);
+  }
 
-  constructor() { }
-
+  fillItems() {
+    let index:number;
+    const names = ['mustard','ginger','garlic','sugar','ravai','cardamom','wheat',
+                'jaggery','gramflour','redgram'];
+    const prices = [75.00,50.00,60.00,38.00,50.00,1100.00,40.00,50.00,70.00,70.00];
+    
+    const items = [];
+    for(index=0;index<names.length;index++) {
+          const item= new Item(index + 1, names[index], prices[index], 1000, null);
+          console.log(item.name)
+          items.push(item);
+          }
+    console.log('in fill');
+    localStorage.setItem("items", JSON.stringify(items));
+  }
 }
--- src/main/webapp/app/entities/ext-bill/ext-item-list/ext-item-list.component.ts
+++ src/main/webapp/app/entities/ext-bill/ext-item-list/ext-item-list.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs/Subscription';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+
+import { Item } from '../../item/item.model';
+// import { ItemService } from './item.service';
+import { ExtBillService} from '../ext-bill.service';
+import { Principal } from '../../../shared';
 
 @Component({
   selector: 'jhi-ext-item-list',
   templateUrl: './ext-item-list.component.html',
   styles: []
 })
-export class ExtItemListComponent implements OnInit {
+export class ExtItemListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  items: Item[];
+  currentAccount: any;
+  eventSubscriber: Subscription;
+
+  constructor(
+        // private itemService: ItemService,
+        private extBillService: ExtBillService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
+  ) { }
 
   ngOnInit() {
+    this.loadAll();
+    this.principal.identity().then((account) => {
+        this.currentAccount = account;
+    });
+    this.registerChangeInItems();
+  }
+
+  ngOnDestroy() {
+    this.eventManager.destroy(this.eventSubscriber);
+  }
+
+  loadAll() {
+    this.items = this.extBillService.fetchItems();
+  }
+
+  trackId(index: number, item: Item) {
+    return item.id;
+  }
+
+  registerChangeInItems() {
+    this.eventSubscriber = this.eventManager.subscribe('itemListModification', (response) => this.loadAll());
+  }
+
+  private onError(error) {
+    this.jhiAlertService.error(error.message, null, null);
   }
 
 }
